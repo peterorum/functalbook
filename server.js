0349be3eb288188b616877e4f274cb9ef502c3d8
Peter Orum <peter@peterorum.com>
2015-03-14 23:32:05 +1100

    add server

---
+++ server.js
@@ -0,0 +1,39 @@
+(function()
+{
+    "use strict";
+
+    // just servers up one particular file
+
+    fs = require('fs');
+    http = require('http');
+    url = require('url');
+
+    http.createServer(function(req, res)
+    {
+        var request = url.parse(req.url, true);
+        var action = request.pathname;
+
+        if (action == '/functal.png')
+        {
+            var img = fs.readFileSync('./functal.png');
+
+            res.writeHead(200,
+            {
+                'Content-Type': 'image/png'
+            });
+
+            res.end(img, 'binary');
+        }
+        else
+        {
+            res.writeHead(200,
+            {
+                'Content-Type': 'text/plain'
+            });
+
+            res.end('Functal\n');
+        }
+    }).listen(8081);
+
+
+})();
