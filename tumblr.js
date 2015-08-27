4483e54fd407838dcc88de048b9dbb36de7ea4c7
Peter Orum <peter@peterorum.com>
2015-08-27 22:37:41 +1000

    title tumblr

--- tumblr.js
+++ tumblr.js
@@ -9,74 +9,82 @@
   var fs = require('fs');
   var fsq = require('../functal/fsq');
   var s3 = require('../functal/s3client');
+  var promise = require("bluebird");
+  var mongodb = promise.promisifyAll(require("mongodb"));
+  var mongoClient = promise.promisifyAll(mongodb.MongoClient);
 
-  // tokens are environment variables
+  mongoClient.connectAsync(process.env.mongo_functal).then(function(client) {
 
-  // Authenticate via OAuth
-  var client = tumblr.createClient(
-    {
-      consumer_key: process.env.tumblr_consumer_key,
-      consumer_secret: process.env.tumblr_consumer_secret,
-      token: process.env.tumblr_token,
-      token_secret: process.env.tumblr_token_secret
-    });
+    var db = client.db('functal');
 
-  var bucket = 'functal-images';
-  var bucketJson = 'functal-json';
+    // tokens are environment variables
 
-  s3.list('functal-images').then(function(result) {
-    if (result.count === 0) {
-      console.log('No files');
-    }
-    else {
-      var r = Math.floor(Math.random() * result.files.length);
-      var key = result.files[r].Key;
+    // Authenticate via OAuth
+    var tumblrClient = tumblr.createClient(
+      {
+        consumer_key: process.env.tumblr_consumer_key,
+        consumer_secret: process.env.tumblr_consumer_secret,
+        token: process.env.tumblr_token,
+        token_secret: process.env.tumblr_token_secret
+      });
 
-      var tmpFile = '/tmp/tumblr-' + key;
+    var bucket = 'functal-images';
+    var bucketJson = 'functal-json';
 
-      s3.download(bucket, key, tmpFile).then(function() {
-        client.userInfo(function(err, data) {
-          if (err) {
-            console.log(err);
-          }
+    s3.list('functal-images').then(function(result) {
+      if (result.count === 0) {
+        console.log('No files');
+      }
+      else {
+        var r = Math.floor(Math.random() * result.files.length);
+        var key = result.files[r].Key;
 
-          // get the blog
-          var df = R.find(R.propEq('name', 'functal'), data.user.blogs);
+        var tmpFile = '/tmp/tumblr-' + key;
 
-          var options = {
-            caption: 'Fractal ' + moment().format('YYYYMMDD.HH') + ' iPhone app https://bit.ly/dailyfunctal',
-            tags: 'fractal,functal',
-            format: 'markdown',
-            link: 'https://functal.tumblr.com', // something required or post fails with 401
-            data: tmpFile
-          };
-
-          client.photo('functal', options, function(err, data) {
+        s3.download(bucket, key, tmpFile).then(function() {
+          tumblrClient.userInfo(function(err, data) {
             if (err) {
               console.log(err);
             }
 
-            console.log(data);
+            // get the blog
+            var df = R.find(R.propEq('name', 'functal'), data.user.blogs);
 
-            fsq.unlink(tmpFile);
+            var msg = "iPhone app https://bit.ly/dailyfunctal";
 
-            // delete image
-            // s3.delete(bucket, key)
-            //     .then(function()
-            //     {
-            //         // delete json
-            //         return s3.delete(bucketJson, key.replace(/(png|jpg)/, 'json'));
-            //     })
-            //     .then(function()
-            //     {
-            //         // delete local file
-            //         return fsq.unlink(tmpFile);
-            //     })
-            //     .done();
+            // prefix msg with title if any
+            db.collection('images').findOneAsync(
+              {
+                name: key
+              }).then(function(image) {
 
+              if (image && image.title) {
+                msg = '"' + image.title + '" ' + msg;
+              }
+
+              var options = {
+                caption: msg,
+                tags: 'fractal,functal',
+                format: 'markdown',
+                link: 'https://functal.tumblr.com', // something required or post fails with 401
+                data: tmpFile
+              };
+
+              tumblrClient.photo('functal', options, function(err, data) {
+                if (err) {
+                  console.log(err);
+                }
+
+                console.log(data);
+
+                fsq.unlink(tmpFile);
+
+                client.close();
+              });
+            });
           });
         });
-      });
-    }
+      }
+    });
   });
 }());
