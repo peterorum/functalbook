036a97e8cea8d2dfe02f6d3d0da1dc9b139241fb
Peter Orum <petorum0@publicisgroupe.net>
2020-01-17 17:34:03 +1100

    update extend.js

--- extend.js
+++ extend.js
@@ -1,7 +1,14 @@
 (function() {
   var graph = require("fbgraph");
-
   // get from facebook graph api explorer page
+
+  // use this to convert a token from explorer page to a long lived one
+
+  // this may be required if my facebook password changes
+
+  // do for both daily functal & daliy jzx.
+  // replace process.env.fb_df_access_token below with the token from explorer page
+
   graph.setAccessToken(process.env.fb_df_access_token);
 
   graph.extendAccessToken(
