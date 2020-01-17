6ea6d799e275f72cdf58f86c2ab6c9bdc7fd787d
Peter Orum <petorum0@publicisgroupe.net>
2020-01-17 13:58:38 +1100

    token

--- extend.js
+++ extend.js
@@ -1,17 +1,17 @@
-(function()
-{
-    var graph = require('fbgraph');
+(function() {
+  var graph = require("fbgraph");
 
-    graph.setAccessToken(process.env.fb_df_access_token);
+  // get from facebook graph api explorer page
+  graph.setAccessToken(process.env.fb_df_access_token);
 
-    graph.extendAccessToken(
+  graph.extendAccessToken(
     {
-        "access_token": process.env.fb_df_access_token,
-        "client_id": process.env.fb_df_app_id,
-        "client_secret": process.env.fb_df_app_secret
-    }, function(err, facebookRes)
-    {
-        console.log(facebookRes);
-    });
-
+      access_token: process.env.fb_df_access_token,
+      client_id: "App Id from FB",
+      client_secret: "from App Settings/Basic"
+    },
+    function(err, facebookRes) {
+      console.log(facebookRes);
+    }
+  );
 })();
